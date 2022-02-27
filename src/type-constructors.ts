ffa6dbce0345508eb23660986f7b9f4246f8f504
Lily Scott <git@suchipi.tv>
2022-02-27 03:12:59 -0700

    Add useful function names to functions returned by type constructors

--- src/type-constructors.ts
+++ src/type-constructors.ts
@@ -1,34 +1,60 @@
 import type { TypeValidator } from "./type-validator";
 import * as basicTypes from "./basic-types";
 
+function objectStr(obj: { [key: string | number | symbol]: string }) {
+  return `{ ${Object.entries(obj)
+    .map(([key, value]) => `${key}: ${value}`)
+    .join(", ")} }`;
+}
+
 export function arrayOf<T>(
   typeValidator: TypeValidator<T>
 ): TypeValidator<Array<T>> {
-  return (target): target is Array<T> =>
+  const ret = (target): target is Array<T> =>
     basicTypes.arrayOfAny(target) && target.every(typeValidator);
+  Object.defineProperty(ret, "name", {
+    value: `arrayOf(${typeValidator.name})`,
+  });
+  return ret;
 }
 
 export function exactString<T extends string>(str: T): TypeValidator<T> {
-  return (target): target is T => target === str;
+  const ret = (target): target is T => target === str;
+  Object.defineProperty(ret, "name", {
+    value: `exactString(${JSON.stringify(str)})`,
+  });
+  return ret;
 }
 
 export function exactNumber<T extends number>(num: T): TypeValidator<T> {
-  return (target): target is T => target === num;
+  const ret = (target): target is T => target === num;
+  Object.defineProperty(ret, "name", {
+    value: `exactNumber(${num})`,
+  });
+  return ret;
 }
 
 export function hasClassName<Name extends string>(
   name: Name
 ): TypeValidator<{ constructor: Function & { name: Name } }> {
-  return (target): target is { constructor: Function & { name: Name } } =>
+  const ret = (target): target is { constructor: Function & { name: Name } } =>
     basicTypes.anyObject(target) &&
     typeof target.constructor === "function" &&
     target.constructor.name === name;
+  Object.defineProperty(ret, "name", {
+    value: `hasClassName(${JSON.stringify(name)})`,
+  });
+  return ret;
 }
 
 export function instanceOf<Klass extends Function & { prototype: any }>(
   klass: Klass
 ): TypeValidator<Klass["prototype"]> {
-  return (target): target is Klass => target instanceof klass;
+  const ret = (target): target is Klass => target instanceof klass;
+  Object.defineProperty(ret, "name", {
+    value: `instanceOf(${JSON.stringify(klass.name)})`,
+  });
+  return ret;
 }
 
 export interface IntersectionFn {
@@ -195,7 +221,11 @@ export interface IntersectionFn {
 export const intersection: IntersectionFn = (
   ...args: Array<TypeValidator<any>>
 ) => {
-  return (target: any): target is any => args.every((arg) => arg(target));
+  const ret = (target: any): target is any => args.every((arg) => arg(target));
+  Object.defineProperty(ret, "name", {
+    value: `intersection(${args.map((arg) => arg.name).join(", ")})`,
+  });
+  return ret;
 };
 
 export const and = intersection;
@@ -362,7 +392,11 @@ export interface UnionFn {
 }
 
 export const union: UnionFn = (...args: Array<TypeValidator<any>>) => {
-  return (target: any): target is any => args.some((arg) => arg(target));
+  const ret = (target: any): target is any => args.some((arg) => arg(target));
+  Object.defineProperty(ret, "name", {
+    value: `union(${args.map((arg) => arg.name).join(", ")})`,
+  });
+  return ret;
 };
 
 export const or = union;
@@ -371,21 +405,39 @@ export function mapOf<K, V>(
   keyType: TypeValidator<K>,
   valueType: TypeValidator<V>
 ): TypeValidator<Map<K, V>> {
-  return (target): target is Map<K, V> =>
+  const ret = (target): target is Map<K, V> =>
     basicTypes.anyMap(target) &&
     Array.from(target.keys()).every(keyType) &&
     Array.from(target.values()).every(valueType);
+
+  Object.defineProperty(ret, "name", {
+    value: `mapOf(${keyType.name}, ${valueType.name})`,
+  });
+
+  return ret;
 }
 
 export function setOf<T>(itemType: TypeValidator<T>): TypeValidator<Set<T>> {
-  return (target): target is Set<T> =>
+  const ret = (target): target is Set<T> =>
     basicTypes.anySet(target) && Array.from(target.entries()).every(itemType);
+
+  Object.defineProperty(ret, "name", {
+    value: `setOf(${itemType.name})`,
+  });
+
+  return ret;
 }
 
 export function maybe<T>(
   itemType: TypeValidator<T>
 ): TypeValidator<T | undefined | null> {
-  return union(itemType, basicTypes.undefined_, basicTypes.null_);
+  const ret = union(itemType, basicTypes.undefined_, basicTypes.null_);
+
+  Object.defineProperty(ret, "name", {
+    value: `maybe(${itemType.name})`,
+  });
+
+  return ret;
 }
 
 export function objectWithProperties<
@@ -397,7 +449,7 @@ export function objectWithProperties<
 }> {
   const propEntries = Object.entries(properties);
 
-  return (
+  const ret = (
     target
   ): target is {
     [key in keyof T]: T[key] extends TypeValidator<infer U> ? U : never;
@@ -407,6 +459,14 @@ export function objectWithProperties<
       propEntries.every(([name, validator]) => validator(target[name]))
     );
   };
+
+  Object.defineProperty(ret, "name", {
+    value: `objectWithProperties(${objectStr(
+      Object.fromEntries(propEntries.map(([key, value]) => [key, value.name]))
+    )})`,
+  });
+
+  return ret;
 }
 
 export function objectWithOnlyTheseProperties<
@@ -419,7 +479,7 @@ export function objectWithOnlyTheseProperties<
   const propEntries = Object.entries(properties);
   const propEntriesSet = new Set(propEntries);
 
-  return (
+  const ret = (
     target
   ): target is {
     [key in keyof T]: T[key] extends TypeValidator<infer U> ? U : never;
@@ -435,6 +495,14 @@ export function objectWithOnlyTheseProperties<
       targetEntries.every((key) => propEntriesSet.has(key))
     );
   };
+
+  Object.defineProperty(ret, "name", {
+    value: `objectWithOnlyTheseProperties(${objectStr(
+      Object.fromEntries(propEntries.map(([key, value]) => [key, value.name]))
+    )})`,
+  });
+
+  return ret;
 }
 
 export function mappingObjectOf<
@@ -444,11 +512,17 @@ export function mappingObjectOf<
   keyType: TypeValidator<Keys>,
   valueType: TypeValidator<Values>
 ): TypeValidator<Record<Keys, Values>> {
-  return (target): target is Record<Keys, Values> =>
+  const ret = (target): target is Record<Keys, Values> =>
     basicTypes.anyObject(target) &&
     Object.entries(target).every(
       ([key, value]) => keyType(key) && valueType(value)
     );
+
+  Object.defineProperty(ret, "name", {
+    value: `mappingObjectOf(${keyType.name}, ${valueType.name})`,
+  });
+
+  return ret;
 }
 
 export const record = mappingObjectOf;
@@ -464,7 +538,7 @@ export function partialObjectWithProperties<
 }> {
   const propEntries = Object.entries(properties);
 
-  return (
+  const ret = (
     target
   ): target is {
     [key in keyof T]: T[key] extends TypeValidator<infer U>
@@ -478,15 +552,40 @@ export function partialObjectWithProperties<
       )
     );
   };
+
+  Object.defineProperty(ret, "name", {
+    value: `partialObjectWithProperties(${objectStr(
+      Object.fromEntries(
+        propEntries.map(([key, value]) => [
+          key,
+          `union(${value.name}, null_, undefined_)`,
+        ])
+      )
+    )})`,
+  });
+
+  return ret;
 }
 
 export function stringMatching(regexp: RegExp): TypeValidator<string> {
-  return (target): target is string =>
+  const ret = (target): target is string =>
     typeof target === "string" && regexp.test(target);
+
+  Object.defineProperty(ret, "name", {
+    value: `stringMatching(${regexp})`,
+  });
+
+  return ret;
 }
 
 export function symbolFor(key: string): TypeValidator<symbol> {
-  return (target): target is symbol => target === Symbol.for(key);
+  const ret = (target): target is symbol => target === Symbol.for(key);
+
+  Object.defineProperty(ret, "name", {
+    value: `symbolFor(${JSON.stringify(key)})`,
+  });
+
+  return ret;
 }
 
 export interface TupleFn {
@@ -659,10 +758,16 @@ export interface TupleFn {
 }
 
 export const tuple: TupleFn = (...args: Array<TypeValidator<any>>) => {
-  return (target: any): target is any =>
+  const ret = (target: any): target is any =>
     basicTypes.anyArray(target) &&
     target.length === args.length &&
     target.every((item, index) => {
       return args[index](item);
     });
+
+  Object.defineProperty(ret, "name", {
+    value: `tuple(${args.map((arg) => arg.name).join(", ")})`,
+  });
+
+  return ret;
 };
