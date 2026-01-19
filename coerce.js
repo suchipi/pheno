10c825ec5b6a8aff91b8eb28375bb10fc87cab45
Lily Skye <git@suchipi.tv>
2026-01-19 13:05:00 -0700

    fix path for backwards-compat root-level coerce.js

--- coerce.js
+++ coerce.js
@@ -1,3 +1,3 @@
-const coerce = require("./dist/coerce");
+const coerce = require("./dist/cjs/coerce");
 module.exports = Object.assign(coerce.default, coerce);
 Object.defineProperty(module.exports, "__esModule", { value: true });
