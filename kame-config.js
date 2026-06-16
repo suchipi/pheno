f9e86494be8e70328cdecc059e6a15f48ce15d35
Lily Skye <git@suchipi.tv>
2026-06-16 00:04:02 -0600

    bump deps

--- kame-config.js
+++ kame-config.js
@@ -1,4 +1,5 @@
-const { defaultLoader } = require("kame");
+const path = require("path");
+const { defaultLoader, defaultResolver } = require("kame");
 
 exports.load = function load(filename) {
   return defaultLoader.load(filename, { target: "es5" });
